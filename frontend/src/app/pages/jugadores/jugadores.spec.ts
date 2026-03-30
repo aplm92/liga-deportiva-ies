32c514303ed69f214587d6668a78eb453666b359
Alberto <alber_513@hotmail.com>
2026-03-30 22:22:39 +0200

    test4

--- frontend/src/app/pages/jugadores/jugadores.spec.ts
+++ frontend/src/app/pages/jugadores/jugadores.spec.ts
@@ -1,24 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { FormsModule } from '@angular/forms';
 import { Jugadores } from './jugadores';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
 
 describe('Jugadores', () => {
   let component: Jugadores;
   let fixture: ComponentFixture<Jugadores>;
+  let httpMock: HttpTestingController;
 
   beforeEach(async () => {
-  await TestBed.configureTestingModule({
-    declarations: [Jugadores],
-    imports: [FormsModule]
-  })
-  .compileComponents();
+    await TestBed.configureTestingModule({
+      imports: [FormsModule, HttpClientTestingModule],
+      declarations: [Jugadores]
+    }).compileComponents();
 
-  fixture = TestBed.createComponent(Jugadores);
-  component = fixture.componentInstance;
-  await fixture.whenStable();
-});
+    fixture = TestBed.createComponent(Jugadores);
+    component = fixture.componentInstance;
+    httpMock = TestBed.inject(HttpTestingController);
+  });
 
   it('should create', () => {
+    fixture.detectChanges();
+
+    const req = httpMock.expectOne('http://localhost:3000/api/jugadores');
+    req.flush([]); // respuesta mock
+
     expect(component).toBeTruthy();
+  });
+
+  afterEach(() => {
+    httpMock.verify(); // asegura que no quedan peticiones pendientes
   });
 });
