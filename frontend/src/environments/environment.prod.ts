d11b82382b1c02bfe6bbf3e743eced923c3e053b
Alberto <alber_513@hotmail.com>
2026-04-25 19:37:35 +0200

    Cambio .env despliegue en Render

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://liga-deportiva-alberto-perez.onrender.com/api'
+  apiUrl: 'https://liga-deportiva-ies-front.onrender.com/api'
 };
